8fe218532b29043b37ee893196293427133445d8
SK <136956949+SathishSK2306@users.noreply.github.com>
2023-08-07 22:27:06 +0530

    Add files via upload

--- cc.py
+++ cc.py
@@ -8,8 +8,8 @@ C = 5
 RA = 0
 gradeList = ['O', 'A+', 'A', 'B+', 'B', 'C', 'RA']
 
-def overall(v1,v2,v3,v4,v5,v6):
-    return (v1+v2+v3+v4+v5+v6)/14
+def overall(v1,v2,v3,v4,v5,v6,v7,v8,v9):
+    return (v1+v2+v3+v4+v5+v6+v7+v8+v9)/22
 
 
 def gradeValue(grade):
@@ -34,54 +34,70 @@ st.set_page_config(layout='wide')
 lay1, lay2 = st.columns([1, 1])
 
 with lay1:
-    s1 = st.selectbox('Whats your Grade Sub1', gradeList)
-    s2 = st.selectbox('Whats your Grade Sub2', gradeList)
-    s3 = st.selectbox('Whats your Grade Sub3', gradeList)
-    s4 = st.selectbox('Whats your Grade Sub4', gradeList)
-    s5 = st.selectbox('Whats your Grade Sub5', gradeList)
-    s6 = st.selectbox('Whats your Grade Sub6', gradeList)
+    s1 = st.selectbox('Edge Computing', gradeList)
+    s2 = st.selectbox('Java Script', gradeList)
+    s3 = st.selectbox('Wireless Communication', gradeList)
+    s4 = st.selectbox('Machine Learning', gradeList)
+    s5 = st.selectbox('Data Communication', gradeList)
+    s6 = st.selectbox('VLSI', gradeList)
+    s7 = st.selectbox('Machine Learning Laboratory', gradeList)
+    s8 = st.selectbox('Networks Laboratory', gradeList )
+    s9 = st.selectbox('Mini Project', gradeList )
+
 
 with lay2:
     col0, col1, col2 = st.columns([0.5, 2, 1])
     with col0:
         st.write("")
     with col1:
-        st.write("Transform and Partial Differential Equation")
-        st.write("Digital Signal Processing")
+        st.write("Edge Computing")
         st.write("Java Script")
-        st.write("Carrer English")
+        st.write("Wireless Communication")
         st.write("Machine Learning")
-        st.write("PHP full stack")
+        st.write("Data Communication")
+        st.write("VLSI")
+        st.write("Machine Learning Laboratory")
+        st.write("Networks Laboratory")
+        st.write("Mini Project")
         st.write(" # result")
 
 
     with col2:
-        st.write(2 * gradeValue(s1))
+        st.write(3 * gradeValue(s1))
         st.write(3 * gradeValue(s2))
-        st.write(2 * gradeValue(s3))
-        st.write(2 * gradeValue(s4))
+        st.write(3 * gradeValue(s3))
+        st.write(3 * gradeValue(s4))
         st.write(3 * gradeValue(s5))
-        st.write(2 * gradeValue(s6))
-        sub1 = 2 * gradeValue(s1)
+        st.write(4 * gradeValue(s6))
+        st.write(1 * gradeValue(s7))
+        st.write(1 * gradeValue(s8))
+        st.write(1 * gradeValue(s9))
+        sub1 = 3 * gradeValue(s1)
         sub2 = 3 * gradeValue(s2)
-        sub3 = 2 * gradeValue(s3)
-        sub4 = 2 * gradeValue(s4)
+        sub3 = 3 * gradeValue(s3)
+        sub4 = 3 * gradeValue(s4)
         sub5 = 3 * gradeValue(s5)
-        sub6 = 2 * gradeValue(s6)
-        total = (sub1 + sub2 + sub3 + sub4 + sub5 + sub6) / 14
+        sub6 = 4 * gradeValue(s6)
+        sub7 = 1 * gradeValue(s7)
+        sub8 = 1 * gradeValue(s8)
+        sub9 = 1 * gradeValue(s9)
+        total = (sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7 + sub8 + sub9) / 22
         rounded_total=round(total,2)
         st.write(f"# {rounded_total}")
         
 
 
 def cal():
-    sub1 = 2 * gradeValue(s1)
+    sub1 = 3 * gradeValue(s1)
     sub2 = 3 * gradeValue(s2)
-    sub3 = 2 * gradeValue(s3)
-    sub4 = 2 * gradeValue(s4)
+    sub3 = 3 * gradeValue(s3)
+    sub4 = 3 * gradeValue(s4)
     sub5 = 3 * gradeValue(s5)
-    sub6 = 2 * gradeValue(s6)
-    total = (sub1 + sub2 + sub3 + sub4 + sub5 + sub6) / 14
+    sub6 = 4 * gradeValue(s6)
+    sub7 = 1 * gradeValue(s7)
+    sub8 = 1 * gradeValue(s8)
+    sub9 = 1 * gradeValue(s9)
+    total = (sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7 + sub8 + sub9) / 22
     print(total)
 
 
